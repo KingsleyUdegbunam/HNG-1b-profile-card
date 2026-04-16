71ebbe17e726e4977cb783dabb8585a6c0bff1fa
kingsley <kingsleykay26@gmail.com>
2026-04-16 21:19:14 +0100

    refactor: adjust time update logic after removing datetime attribute

--- js/main.js
+++ js/main.js
@@ -2,12 +2,7 @@ const timeEl = document.querySelector(".time-display");
 
 function updateTime() {
   const epochNow = Date.now();
-  const dateNow = new Date();
-
   timeEl.textContent = epochNow;
-  timeEl.setAttribute("datetime", dateNow.toISOString());
-
-  console.log(dateNow.toISOString());
 }
 
 updateTime();
